ffb97c257bacaeee9ba137f4199751bc942fb97c
shivk <shivk@LAPTOP-MHUR3DA6>
2022-10-07 13:13:28 +0530

    First Commit....

---
+++ src/Example/Test1.java
@@ -0,0 +1,14 @@
+package Example;
+
+public class Test1 {
+	public static void main(String[] args) {
+
+		String hungry = "yes";
+		if (hungry == "yes")
+			System.out.println("eat Something....");
+		else {
+			System.out.println("Do work....");
+		}
+	}
+
+}
